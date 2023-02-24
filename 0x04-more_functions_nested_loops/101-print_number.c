0cda691fae05e2f0e4ad594c2a528fcb3f3c7205
LeighAnne17 <leighndimande17@icloud.com>
2023-02-23 19:45:31 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,13 +11,16 @@ void print_number(int n)
 {
 	unsigned int k = n;
 
-	if ( n< 0)
+	if (n < 0)
 	{
 	n == -1;
 	k = n;
 	_putchar('-';
 	}
+	else
+	{
 	k /= 10;
+	}
 	if (k != 0)
 	print_number(l);
 	_putchar((unsigned int) n % 10 + '0');
