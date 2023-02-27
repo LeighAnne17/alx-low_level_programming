c6de94887c1d67fb3308e364bb1a7798f7ecd08a
LeighAnne17 <leighndimande17@icloud.com>
2023-02-27 15:41:21 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: the string to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c]  != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		putchar(s[c]);
+	}
+	putchar('\n');
+}
