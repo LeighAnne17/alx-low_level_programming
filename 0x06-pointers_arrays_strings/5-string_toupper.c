f2b929c2e94c6e782f4f0e1422f574ba7ae2d0ae
LeighAnne17 <leighndimande17@icloud.com>
2023-03-02 16:31:59 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-main.c 5-string_toupper.c -o 5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,16 @@
 
 char *string_toupper(char *str)
 {
-	int index = 0;
+	int i;
 
-	while (str[index])
+	i = 0;
+
+	while (str[i] != '\0')
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-		{
-			str[index] -= 32;
-			index++;
-		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+
+		i++;
 	}
 	return (str);
 }
