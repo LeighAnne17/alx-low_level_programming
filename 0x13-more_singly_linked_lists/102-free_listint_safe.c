299da15402815b148493098fe99aa076ea0cbcce
LeighAnne17 <leighndimande17@icloud.com>
2023-04-04 19:32:11 -0700

    prints linked list and returns the number of nodes in the list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,57 @@
 #include "lists.h"
 
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
+/**
+ * looped_listint_count - counts the number of unique codes
+ * in looped linked list
+ * @head: poniter to head of linked list
+ * Return: number of unique codes
+ */
+
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *frog, *chameleon;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	frog = head->next;
+	chameleon = (head->next)->next;
+
+	while (chameleon)
+	{
+		if (frog == chameleon)
+		{
+			frog = head;
+
+			while (frog != chameleon)
+			{
+				nodes++;
+				frog = frog->next;
+				chameleon = chameleon->next;
+			}
+
+			frog = frog->next;
+			while (frog != chameleon)
+			{
+				nodes++;
+				frog = frog->next;
+			}
+
+			return (nodes);
+		}
+
+		frog = frog->next;
+		chameleon = (chameleon->next)->next;
+	}
+
+	return (0);
+}
+
+
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
@@ -8,33 +60,31 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t len = 0;
-	int difference;
+	size_t nodes, index;
 	listint_t *temporary;
 
-	if (!h || !*h)
-	return (0);
+	nodes = looped_listint_count(*h);
 
-	while (*h)
+	if (nodes == 0)
 	{
-		difference = *h - (*h)->next;
-
-		if (difference > 0)
+		for (; h != NULL && *h != NULL; nodes++)
 		{
 			temporary = (*h)->next;
 			free(*h);
 			*h = temporary;
-			len++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
-			break;
 		}
 	}
-	*h = NULL;
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			temporary = *h->next;
+			free(*h);
+			*h = temporary;
+		}
+		*h = NULL;
+	}
+	h = NULL;
 
-	return (len);
+	return (nodes);
 }
