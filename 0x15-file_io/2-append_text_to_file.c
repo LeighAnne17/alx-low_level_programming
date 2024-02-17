ab5938785a909ee7ef06ecfc605f212f48fea73e
Leigh17 <leighndimande17@iclod.com>
2024-02-16 19:24:35 -0800

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text
+ * @filename: name of file
+ * @text_content: NULL terminated string
+ * Return: 1 on success and -1 on fsilure
+ */
+
+int append_text_to_file(const char *filename, char *text_content);
+{
+	int o, write, length = 0;
+
+	if (filename == NULL)
+		reurn (1);
+
+	if (text_content != NULL)
+	{
+		for (length =0; text_content[length];)
+			length++;
+	}
+
+	o = open(filename, O_WRONLY| O_APPEND);
+	write = write(o, text_content, length);
+
+	if ( o == -1 || write == -1)
+		return (1);
+
+	close(o);
+	return (1);
+}
